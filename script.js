501efad9520da95f644b3ee4eec55ceecbfe4919
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-25 23:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+const ip = document.getElementById('ip');
+const output = document.getElementById('output');
+
+function createPromise(n , delay){
+	return new Promise((res , rej) => {
+		setTimeout(() => {
+			res(n);
+		},delay*1000);
+	});
+}
+
+btn.addEventListener('click' , () =>{
+	let n = ip.value;
+	if(n){
+		createPromise(n,2).then( num => {
+			output.innerText = `Result: ${num}`;
+			return createPromise(num*2 , 1);
+		}).then( num => {
+			output.innerText = `Result: ${num}`;
+			return createPromise(num-3 , 1);
+		}).then( num => {
+			output.innerText = `Result: ${num}`;
+			return createPromise(num/2 , 1);
+		}).then( num => {
+			output.innerText = `Result: ${num}`;
+			return createPromise(num+10, 1);
+		}).then( num => {
+			output.innerText = `Result: ${num}`;
+		})
+	}
+});
